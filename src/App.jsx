4aeea073308b8d54adb23646ce387f0717c3c992
Hansel-svg <thinhduong293@gmail.com>
2026-05-05 22:29:55 +0700

    basic sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
+import "./App.css";
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 function App() {
   const [instruments, setInstruments] = useState([]);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState(""); 
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(""); 
+  const [signUpError, setSignUpError] = useState("");
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+
 
   useEffect(() => {
     getInstruments();
@@ -15,12 +23,40 @@ function App() {
     setInstruments(data);
   }
 
+  async function signUp(formData) {
+    formData.preventDefault()
+    if(password !== confirmPassword){
+      setSignUpError("Passwords do not match")
+      return
+    }
+    const { data, error } = await supabase.auth.signUp({
+      email: email,            
+      password: password,
+    })
+    if (error) {
+      setSignUpError(error.message)
+    } else {
+      setSignUpError("Account created successfully!")
+      setSignUpSuccess(true)
+    }
+  } 
+  
+
   return (
-    <ul>
-      {instruments.map((instrument) => (
-        <li key={instrument.name}>{instrument.name}</li>
-      ))}
-    </ul>
+    <>
+      <form onSubmit={signUp} action={signUp}>
+        <input type="email" placeholder="Enter email" onChange={(e) => {setEmail(e.target.value)}}/>
+        <input type="text" placeholder="Enter name" onChange={(e) => {setName(e.target.value)}}/>
+        <input type="password" placeholder="Enter password" onChange={(e) => {setPassword(e.target.value)}}/>
+        <input type="password" placeholder="Confirm password" onChange={(e) => {setConfirmPassword(e.target.value)}}/>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <div className={`${signUpSuccess ? 'successful-message' : 'error-message'}`} >
+        {signUpError}
+      </div>
+    </>
   );
 }
 
