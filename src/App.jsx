526df25c758dde3e3685c6daf9e3f7b896bc0808
Hansel-svg <thinhduong293@gmail.com>
2026-05-06 21:06:21 +0700

    SignUp component creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,18 @@
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
 import "./App.css";
-
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
-
-function App() {
-  const [instruments, setInstruments] = useState([]);
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState(""); 
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState(""); 
-  const [signUpError, setSignUpError] = useState("");
-  const [signUpSuccess, setSignUpSuccess] = useState(false);
+import { Route } from "wouter";
+import SignUp from "./SignUp";
 
 
-  async function signUp(formData) {
-    formData.preventDefault()
-    if(password !== confirmPassword){
-      setSignUpError("Passwords do not match")
-      return
-    }
-    const { data, error } = await supabase.auth.signUp({
-      email: email,            
-      password: password,
-      options: {
-        data: {
-          username: name
-        }
-      }
-    })
-    if (error) {
-      setSignUpError(error.message)
-    } else {
-      setSignUpError("Account created successfully!")
-      setSignUpSuccess(true)
-    }
-  } 
+function App() { 
+  
   
 
   return (
     <>
-      <form onSubmit={signUp} action={signUp}>
-        <input type="email" required placeholder="Enter email" onChange={(e) => {setEmail(e.target.value)}}/>
-        <input type="text" required placeholder="Enter name" onChange={(e) => {setName(e.target.value)}}/>
-        <input type="password" required placeholder="Enter password" onChange={(e) => {setPassword(e.target.value)}}/>
-        <input type="password" required placeholder="Confirm password" onChange={(e) => {setConfirmPassword(e.target.value)}}/>
-
-        <button type="submit">Submit</button>
-      </form>
-
-      <div className={`${signUpSuccess ? 'successful-message' : 'error-message'}`} >
-        {signUpError}
-      </div>
+      <Route path="/signup">
+        <SignUp />
+      </Route>
     </>
   );
 }
---
+++ src/SignUp.jsx
@@ -0,0 +1,54 @@
+import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
+function SignUp() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState(""); 
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(""); 
+  const [signUpError, setSignUpError] = useState("");
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+
+  async function signUp(formData) {
+    formData.preventDefault()
+    if(password !== confirmPassword){
+      setSignUpError("Passwords do not match")
+      return
+    }
+    const { data, error } = await supabase.auth.signUp({
+      email: email,            
+      password: password,
+      options: {
+        data: {
+          username: name
+        }
+      }
+    })
+    if (error) {
+      setSignUpError(error.message)
+    } else {
+      setSignUpError("Account created successfully!")
+      setSignUpSuccess(true)
+    }
+  }
+
+  return (
+        <>
+            <form onSubmit={signUp} action={signUp}>
+            <input type="email" required placeholder="Enter email" onChange={(e) => {setEmail(e.target.value)}}/>
+            <input type="text" required placeholder="Enter name" onChange={(e) => {setName(e.target.value)}}/>
+            <input type="password" required placeholder="Enter password" onChange={(e) => {setPassword(e.target.value)}}/>
+            <input type="password" required placeholder="Confirm password" onChange={(e) => {setConfirmPassword(e.target.value)}}/>
+
+            <button type="submit">Submit</button>
+            </form>
+
+            <div className={`${signUpSuccess ? 'successful-message' : 'error-message'}`} >
+            {signUpError}
+            </div>
+        </>
+  );
+}
+
+export default SignUp;
